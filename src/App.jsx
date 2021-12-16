7298f62ce78e4683b12ca21a879c79009962d5ea
Konton022 <42418105+Konton022@users.noreply.github.com>
2021-12-17 00:33:25 +0500

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from './layout/Footer';
+import Header from './layout/Header';
+import Main from './layout/Main';
+
+export default class App extends React.Component {
+  render() {
+    return (<>
+      <Header />
+      <Main />
+      <Footer />
+    </>);
+  }
+}
---
+++ src/layout/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<nav className='light-blue darken-3'>
+    		<div className="nav-wrapper">
+      		<a href="#" className="brand-logo left">React Movies</a>
+      		<ul id="nav-mobile" className="right">
+        		<li><a href="#">Repo</a></li>
+      		</ul>
+    		</div>
+  		</nav>
+	);
+}
+
+export default Header;
